34f5e13de9de8b10f3b25ee59fd047c626018aee
bphillips <bphillips@unknown>
2013-07-03 17:15:09 +0000

    Added annotations example app
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk/struts2examples@1499480 13f79535-47bb-0310-9956-ffa450edef68

---
+++ annotations/src/main/java/org/apache/struts/actions/HelloAction.java
@@ -0,0 +1,48 @@
+package org.apache.struts.actions;
+
+import org.apache.log4j.Logger;
+
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Acts as a controller to handle actions
+ * related to registering a user.
+ * @author bruce phillips
+ *
+ */
+public class HelloAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = Logger.getLogger( HelloAction.class.getName() );
+	
+	private String message;
+	
+		
+	public String execute() throws Exception {
+		
+		logger.info("In execute method of class Hello");
+		
+		message = "Hello from Struts 2 with no XML configuration.";
+		
+		return SUCCESS;
+		
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+	
+
+
+	
+
+
+}
---
+++ annotations/src/main/java/org/apache/struts/actions/RegisterAction.java
@@ -0,0 +1,57 @@
+package org.apache.struts.actions;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.annotations.model.Person;
+import org.apache.struts2.convention.annotation.Action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Acts as a controller to handle actions
+ * related to registering a user.
+ * @author bruce phillips
+ *
+ */
+public class RegisterAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = Logger.getLogger( RegisterAction.class.getName() );
+	
+	private Person personBean;
+	
+	@Action("register-input")
+	public String input() throws Exception {
+
+		logger.info("In input method of class RegisterAction");
+		
+		return INPUT;
+
+	}
+
+	public String execute() throws Exception {
+		
+		logger.info("In execute method of class RegisterAction");
+		
+		//call Service class to store personBean's state in database
+		
+		return SUCCESS;
+		
+	}
+	
+
+
+	
+	public Person getPersonBean() {
+		
+		return personBean;
+		
+	}
+	
+	public void setPersonBean(Person person) {
+		
+		personBean = person;
+		
+	}
+
+}
