8d530157fed0df9442fb897478195889428de0f3
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-04-17 15:40:37 +0200

    removed unnecessary setter

--- helloworld/src/main/java/org/apache/struts/helloworld/model/MessageStore.java
+++ helloworld/src/main/java/org/apache/struts/helloworld/model/MessageStore.java
@@ -10,15 +10,11 @@ public class MessageStore {
     private String message;
     
     public MessageStore() {
-        setMessage("Hello Struts User");
+        message = "Hello Struts User";
     }
 
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
 }
