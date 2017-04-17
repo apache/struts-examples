06c4deda76ae90512db8b2f246d89e3a417b86cc
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-04-17 15:22:24 +0200

    changed tabs to spaces

--- helloworld/src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
+++ helloworld/src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
@@ -14,35 +14,33 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class HelloWorldAction extends ActionSupport {
 
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 * The model class that stores the message
-	 * to display in the view.
-	 */
-	private MessageStore messageStore;
-	
-	/*
-	 * Creates the MessageStore model object and 
-	 * returns success.  The MessageStore model
-	 * object will be available to the view.
-	 * (non-Javadoc)
-	 * @see com.opensymphony.xwork2.ActionSupport#execute()
-	 */
-	public String execute() throws Exception {
-		
-		messageStore = new MessageStore() ;
-		return SUCCESS;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public MessageStore getMessageStore() {
-		return messageStore;
-	}
+    /**
+     * The model class that stores the message
+     * to display in the view.
+     */
+    private MessageStore messageStore;
 
-	public void setMessageStore(MessageStore messageStore) {
-		this.messageStore = messageStore;
-	}
-	
-	
+    /*
+     * Creates the MessageStore model object and 
+     * returns success.  The MessageStore model
+     * object will be available to the view.
+     * (non-Javadoc)
+     * @see com.opensymphony.xwork2.ActionSupport#execute()
+     */
+    public String execute() throws Exception {
+        
+        messageStore = new MessageStore() ;
+        return SUCCESS;
+    }
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+
+    public void setMessageStore(MessageStore messageStore) {
+        this.messageStore = messageStore;
+    }
 
 }
--- helloworld/src/main/java/org/apache/struts/helloworld/model/MessageStore.java
+++ helloworld/src/main/java/org/apache/struts/helloworld/model/MessageStore.java
@@ -6,20 +6,19 @@ package org.apache.struts.helloworld.model;
  *
  */
 public class MessageStore {
-	
-	private String message;
-	
-	public MessageStore() {
-		
-		setMessage("Hello Struts User");
-	}
+    
+    private String message;
+    
+    public MessageStore() {
+        setMessage("Hello Struts User");
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
 }
